5e12f875a4e08256d4d2116db9108fce499cc45a
Dmitriy Samarin <Samarin99@gmail.com>
2026-03-03 23:35:16 +0200

    Updated Tests

--- src/Tests/UtilsTests.cpp
+++ src/Tests/UtilsTests.cpp
@@ -22,7 +22,7 @@ void TestAABBCollisionCheck()
 		CollisionBox aBox{ glm::vec2( 10.0f, 2.0f ), glm::vec2( 12.0f, 8.0f ) };
 		CollisionBox bBox{ glm::vec2( 1.0f, 5.0f ),glm::vec2( 8.0f, 10.0f ) };
 
-		ASSERT( Utils::CheckAABBCollision( aBox, bBox ) );
+		ASSERT( !Utils::CheckAABBCollision( aBox, bBox ) );
 	}
 }
 
